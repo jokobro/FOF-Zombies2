33ca6d918893558ff839fbece4721abbd9d2500a
jokobro <jochemstarwarsclonewars@gmail.com>
2025-01-16 11:58:54 +0100

    added navmesh for zombie and some 3d models

--- Assets/Scripts/Enemy's/BaseEnemy.cs
+++ Assets/Scripts/Enemy's/BaseEnemy.cs
@@ -1,17 +1,31 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class BaseEnemy : MonoBehaviour, IDamageable
-{
+{    
     [SerializeField] private List<GameObject> pickups;
     GameManager gameManager;
     public int pointsAmount;
     public float health;
+    private NavMeshAgent agent = null;
+    [SerializeField] private Transform target;
 
     private void Awake()
     {
+        agent = GetComponent<NavMeshAgent>();
         gameManager = FindObjectOfType<GameManager>();
+    }
+
+    private void Update()
+    {
+        MoveToTarget();
+    }
+
+    private void MoveToTarget()
+    {
+        agent.SetDestination(target.position);
     }
 
     public void TakeDamage(float damage)
--- Assets/Scripts/Managers/WaveManager.cs
+++ Assets/Scripts/Managers/WaveManager.cs
@@ -4,15 +4,12 @@ using UnityEngine;
 
 public class WaveManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public int CurrentWaveIndex { get; private set; } = 0;
+    /*public List<Wave> waves;  // Lijst van waves*/
+    public static WaveManager Instance;
+    private int currentGroupIndex = 0;
+    public float spawnInterval = 30f; // Tussen de groepen in seconden
+    private bool waveActive = false;
+    public float groupCompletionTime = 1f;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
